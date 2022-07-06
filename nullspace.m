852c09099a40595b05362dfdd7e757d686a05e19
HiraMohammedK <98226271+HiraMohammedK@users.noreply.github.com>
2022-07-06 16:27:10 +0530

    Add files via upload

---
+++ nullspace.m
@@ -0,0 +1,36 @@
+﻿A=input('Input the vectors as the columns of a matrix : ');
+disp(' ');
+
+disp('A =');
+disp(A);
+
+[V,D]=eig(A);
+ 
+[m,n]=size(A);
+N=zeros(m,n);
+
+j=1;
+for i=1:n
+    if round(D(i,i))==0
+        N(:,j)=V(:,i);
+        j=j+1;
+    end
+end
+
+k=n-j+1;
+r=1;
+while(r<=k)
+    if (N(:,j)')*N(:,j)==0
+        N(:,j)=[];
+        r=r+1;
+    end
+end
+
+
+
+disp('Null Space,N =');
+disp(N);
+
+disp('A*N =');
+disp(round(A*N));
+
